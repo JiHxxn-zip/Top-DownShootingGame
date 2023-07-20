15a6eb2e3dcb4b2fe0a6a7a8afb94b23a07b8232
MuMuKim <67031535+MuMuKim@users.noreply.github.com>
2023-07-20 20:54:13 +0900

    [230720] Ji 1. Player 이동로직 구현 (Part 1)

---
+++ Assets/02.Scripts/Player.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5;
+
+    private PlayerController controller;
+
+    private Camera viewCamera;
+
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+
+        // 만들어진 moveVelocity를 PlayerController에 전달해서 물리적인 부분들을 처리한다.\
+        controller.Move(moveVelocity);
+
+        // 화면상에서 마우스의 위치를 반환해줌
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+
+        // 코드상으로 Plain을 생성 후 법선 벡터를 넣음(플레인의 수직으 통과함)
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        // 카메라에서 ray가 충돌한 지점까지의 거리
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            //Debug.DrawLine(ray.origin, point, Color.red);
+
+            controller.LookAt(point);
+        }
+    }
+}
---
+++ Assets/02.Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 velocity;
+
+    private Rigidbody myRigidbody;
+
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        // Y값은 고정한 체 로테이션 되도록
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
